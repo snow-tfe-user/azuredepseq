ad8f46c06b9a39dacfaab1789ec6433366abe53e
snow-tfe-user <69755779+snow-tfe-user@users.noreply.github.com>
2020-10-20 16:56:21 +0530

    Update depseqtemplate.tf

--- depseqtemplate.tf
+++ depseqtemplate.tf
@@ -86,14 +86,14 @@ resource "azurerm_linux_virtual_machine" "vml" {
   location              = "${azurerm_resource_group.rg.location}"
   resource_group_name   = "${azurerm_resource_group.rg.name}"
   network_interface_ids = [azurerm_network_interface.nic[1].id]
-  vm_size               = "Standard_DS1_v2"
+  size                  = "Standard_DS1_v2"
 
   admin_username = "testadmin"
   admin_password = "Password1234!"
 
   disable_password_authentication = false
 
-  storage_image_reference {
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
@@ -111,11 +111,11 @@ resource "azurerm_windows_virtual_machine" "vmw" {
   location              = "${azurerm_resource_group.rg.location}"
   resource_group_name   = "${azurerm_resource_group.rg.name}"
   network_interface_ids = [azurerm_network_interface.nic[2].id]
-  vm_size               = "Standard_D1_v2"
+  size                  = "Standard_D1_v2"
   admin_username        = "adminuser"
   admin_password        = "P@$$w0rd1234!"
 
-  storage_image_reference {
+  source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
     sku       = "2012-R2-Datacenter"
